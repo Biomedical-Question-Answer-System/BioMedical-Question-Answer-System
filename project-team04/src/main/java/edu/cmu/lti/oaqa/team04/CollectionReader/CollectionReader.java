6ff25824f0cf88217fd80e4344ae40bd92acf79e
rsun07 <rsun2014@gmail.com>
2014-11-26 14:43:59 -0500

    after finish snippet annotator and the snippet annotator evaluator

--- project-team04/src/main/java/edu/cmu/lti/oaqa/team04/CollectionReader/CollectionReader.java
+++ project-team04/src/main/java/edu/cmu/lti/oaqa/team04/CollectionReader/CollectionReader.java
@@ -1,15 +1,86 @@
 package edu.cmu.lti.oaqa.team04.CollectionReader;
 
-import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
-import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.jcas.JCas;
+import static java.util.stream.Collectors.toList;
 
-public class CollectionReader extends JCasAnnotator_ImplBase {
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import json.JsonCollectionReaderHelper;
+import json.gson.Question;
+import json.gson.TrainingSet;
+
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.collection.CollectionReader_ImplBase;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Progress;
+import org.apache.uima.util.ProgressImpl;
+
+public class CollectionReader extends CollectionReader_ImplBase {
+
+  public static final String PARAM_INPUTPATH = "InputFile";
+
+  private List<Question> inputs;
+
+  private int NumofQues;
+
+  private String oPath;
+
+  private int CurrPos;
+
+  public void initialize() throws ResourceInitializationException {
+    oPath = ((String) getConfigParameterValue(PARAM_INPUTPATH)).trim();
+    CurrPos = 0;
+    // extract the input questions from file
+    System.out.println(oPath);
+
+    Object value = oPath;
+
+    if (String.class.isAssignableFrom(value.getClass()))
+      inputs = TrainingSet.load(getClass().getResourceAsStream(String.class.cast(value))).stream()
+              .collect(toList());
+    else if (String[].class.isAssignableFrom(value.getClass()))
+      inputs = Arrays.stream(String[].class.cast(value))
+              .flatMap(path -> TrainingSet.load(getClass().getResourceAsStream(oPath)).stream())
+              .collect(toList());
+
+    // trim question texts
+    inputs.stream().filter(input -> input.getBody() != null)
+            .forEach(input -> input.setBody(input.getBody().trim().replaceAll("\\s+", " ")));
+
+    NumofQues = inputs.size();
+  }
 
   @Override
-  public void process(JCas aJCas) throws AnalysisEngineProcessException {
-    // TODO Auto-generated method stub
+  public void getNext(CAS aCAS) throws IOException, CollectionException {
+    JCas jcas;
+    try {
+      jcas = aCAS.getJCas();
+    } catch (CASException e) {
+      throw new CollectionException(e);
+    }
+    // add all the information to CAS using helper
+    Question question = inputs.get(CurrPos++);
+    JsonCollectionReaderHelper.addQuestionToIndex(question, "", jcas);
 
+    jcas.setDocumentText(question.getBody());
+  }
+
+  @Override
+  public boolean hasNext() throws IOException, CollectionException {
+    return CurrPos < inputs.size();
+  }
+
+  @Override
+  public Progress[] getProgress() {
+    return new Progress[] { new ProgressImpl(CurrPos, inputs.size(), Progress.ENTITIES) };
+  }
+
+  @Override
+  public void close() throws IOException {
   }
 
 }
--- project-team04/src/main/java/edu/cmu/lti/oaqa/team04/casconsumer/CasConsumer.java
+++ project-team04/src/main/java/edu/cmu/lti/oaqa/team04/casconsumer/CasConsumer.java
@@ -1,15 +1,125 @@
 package edu.cmu.lti.oaqa.team04.casconsumer;
 
-import org.apache.uima.cas.CAS;
-import org.apache.uima.collection.CasConsumer_ImplBase;
-import org.apache.uima.resource.ResourceProcessException;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.collection.CasConsumer_ImplBase;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.ResourceProcessException;
+import org.apache.uima.util.ProcessTrace;
+
+import edu.cmu.lti.oaqa.type.input.Question;
+import edu.cmu.lti.oaqa.type.retrieval.ConceptSearchResult;
+import edu.cmu.lti.oaqa.type.retrieval.Document;
+import edu.cmu.lti.oaqa.type.retrieval.Passage;
+import edu.cmu.lti.oaqa.type.retrieval.TripleSearchResult;
+
+/**
+ * @author alfie
+ *
+ */
 public class CasConsumer extends CasConsumer_ImplBase {
 
+  private PrintWriter writer = null;
+
+  public void initialize() throws ResourceInitializationException {
+    try {
+      writer = new PrintWriter("answer.txt", "UTF-8");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    writer.println("\"questions\": [");
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.apache.uima.collection.base_cpm.CasObjectProcessor#processCas(org.apache.uima.cas.CAS)
+   */
   @Override
   public void processCas(CAS aCAS) throws ResourceProcessException {
     // TODO Auto-generated method stub
+    JCas jcas;
+    try {
+      jcas = aCAS.getJCas();
+    } catch (CASException e) {
+      throw new ResourceProcessException(e);
+    }
+    Question question = new Question(jcas);
+    FSIterator<?> questionIt = jcas.getJFSIndexRepository().getAllIndexedFS(Question.type);
+    if (questionIt.hasNext()) {
+      question = (Question) questionIt.next();
+    }
+    writer.printf("\t{\n");
+    String body = jcas.getDocumentText();
+    writer.printf("\t\t\"body\": \"%s\",\n", body);
+    // concepts
+    writer.printf("\t\t\"concepts\": [");
+    // FSIterator it = jcas.getAnnotationIndex(Question.type).iterator();
+    FSIterator<?> conceptIt = jcas.getJFSIndexRepository()
+            .getAllIndexedFS(ConceptSearchResult.type);// jcas.getAnnotationIndex(Concept.type).iterator();
+    while (conceptIt.hasNext()) {
+      ConceptSearchResult concept = (ConceptSearchResult) conceptIt.next();
+      writer.printf("\n\t\t\t\"%s\"", concept.getUri());
+      if (conceptIt.hasNext())
+        writer.printf(",");
+    }
+    writer.printf("\n\t\t],\n");
+    // documents
+    writer.printf("\t\t\"documents\": [");
+    FSIterator docIt = jcas.getJFSIndexRepository().getAllIndexedFS(Document.type);
+    while (docIt.hasNext()) {
+      Document doc = (Document) docIt.next();
+      writer.printf("\n\t\t\t\"%s\"", doc.getTitle());
+      if (conceptIt.hasNext())
+        writer.printf(",");
+    }
+    writer.printf("\n\t\t],\n");
+    // triples
+    writer.printf("\t\t\"triples\": [");
+    FSIterator tripleIt = jcas.getJFSIndexRepository().getAllIndexedFS(TripleSearchResult.type);
+    while (tripleIt.hasNext()) {
+      TripleSearchResult triple = (TripleSearchResult) tripleIt.next();
+      writer.printf(
+              "\n\t\t\t{\n\t\t\t\t\"o\": \"%s\"\n\t\t\t\t\"p\": \"%s\"\n\t\t\t\t\"s\": \"%s\"\n\t\t\t}",
+              triple.getTriple().getObject(), triple.getTriple().getPredicate(), triple.getTriple()
+                      .getSubject());
+      if (conceptIt.hasNext())
+        writer.printf(",");
+    }
+    writer.printf("\n\t\t],\n");
+    // question id
+    writer.printf("\t\t\"id\": \"" + question.getId() + "\",\n");
+    // the end of the question
+    writer.printf("\t},\n");
+    // snippets
+    writer.printf("\t\t\"triples\": [");
+    FSIterator snippetIt = jcas.getJFSIndexRepository().getAllIndexedFS(Passage.type);
+    while (snippetIt.hasNext()) {
+      Passage snippet = (Passage) snippetIt.next();
+      writer.printf(
+              "\n\t\t\t{\n\t\t\t\t\"beginSection\": \"%s\"\n\t\t\t\t\"document\": \"%s\"\n\t\t\t\t\"endSection\": \"%s\"\n\t\t\t\t\"offsetInBeginSection\": \"%s\"\n\t\t\t\t\"offsetInEndSection\": \"%s\"\n\t\t\t\t\"text\": \"%s\"\n\t\t\t}",
+              snippet.getBeginSection(),snippet.getTitle(),snippet.getEndSection(),snippet.getOffsetInBeginSection(),snippet.getOffsetInEndSection(),snippet.getText());
+      if (conceptIt.hasNext())
+        writer.printf(",");
+    }
+    writer.printf("\n\t\t],\n");
+    // question id
+    writer.printf("\t\t\"id\": \"" + question.getId() + "\",\n");
+    // the end of the question
+    writer.printf("\t},\n");
+  }
 
+  public void collectionProcessComplete(ProcessTrace arg0) throws ResourceProcessException,
+          IOException {
+
+    super.collectionProcessComplete(arg0);
+    writer.println("]}");
+    writer.close();
   }
 
 }
