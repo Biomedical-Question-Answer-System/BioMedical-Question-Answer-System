1023053360ed30fb31f72ccd867c8c1e7bac05a3
rsun07 <rsun2014@gmail.com>
2015-03-27 22:43:20 -0400

    update format

--- project-team04/src/main/java/edu/cmu/lti/oaqa/team04/AaeBIOQA.java
+++ project-team04/src/main/java/edu/cmu/lti/oaqa/team04/AaeBIOQA.java
@@ -1,4 +1,3 @@
-package edu.cmu.lti.oaqa.team04;
 
 import static java.util.stream.Collectors.toList;
 
@@ -44,67 +43,59 @@ import edu.cmu.lti.oaqa.type.retrieval.TripleSearchResult;
 import util.TypeFactory;
 
 public class AaeBIOQA {
-  public static void main(String [] args) 
-          throws Exception {
-          
-        String sLine;
-        long startTime=System.currentTimeMillis();
-        
-        URL descUrl = AaeBIOQA.class.getResource("/descriptors/AaeDescriptor.xml");
-         if (descUrl == null) {
-            throw new IllegalArgumentException("Error opening AaeDescriptor.xml");
-         }
-        // create AnalysisEngine    
-        XMLInputSource input = new XMLInputSource(descUrl);
-        AnalysisEngineDescription desc = UIMAFramework.getXMLParser().parseAnalysisEngineDescription(input);
-        AnalysisEngine anAnalysisEngine = UIMAFramework.produceAnalysisEngine(desc);
-        CAS aCas = anAnalysisEngine.newCAS();
+  public static void main(String[] args) throws Exception {
 
-        
-        String filePath = "/questions.json";
-        List<TestQuestion> inputs;
-        inputs = Lists.newArrayList();
-        /*  InputStream stream = getClass().getResourceAsStream(filePath);
-        try {
-          System.out.println("stream " + stream.read());
-        } catch (IOException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }*/
-        Object value = filePath;
-        if (String.class.isAssignableFrom(value.getClass())) {
-          inputs = TestSet.load(value.getClass().getResourceAsStream(
-                  String.class.cast(value))).stream()
+    String sLine;
+    long startTime = System.currentTimeMillis();
+
+    URL descUrl = AaeBIOQA.class.getResource("/descriptors/AaeDescriptor.xml");
+    if (descUrl == null) {
+      throw new IllegalArgumentException("Error opening AaeDescriptor.xml");
+    }
+    // create AnalysisEngine
+    XMLInputSource input = new XMLInputSource(descUrl);
+    AnalysisEngineDescription desc = UIMAFramework.getXMLParser().parseAnalysisEngineDescription(
+            input);
+    AnalysisEngine anAnalysisEngine = UIMAFramework.produceAnalysisEngine(desc);
+    CAS aCas = anAnalysisEngine.newCAS();
+
+    String filePath = "/questions.json";
+    List<TestQuestion> inputs;
+    inputs = Lists.newArrayList();
+    /*
+     * InputStream stream = getClass().getResourceAsStream(filePath); try {
+     * System.out.println("stream " + stream.read()); } catch (IOException e) { // TODO
+     * Auto-generated catch block e.printStackTrace(); }
+     */
+    Object value = filePath;
+    if (String.class.isAssignableFrom(value.getClass())) {
+      inputs = TestSet.load(value.getClass().getResourceAsStream(String.class.cast(value)))
+              .stream().collect(toList());
+    } else if (String[].class.isAssignableFrom(value.getClass())) {
+      inputs = Arrays.stream(String[].class.cast(value))
+              .flatMap(path -> TestSet.load(value.getClass().getResourceAsStream(path)).stream())
               .collect(toList());
-        } else if (String[].class.isAssignableFrom(value.getClass())) {
-          inputs = Arrays
-              .stream(String[].class.cast(value))
-              .flatMap(
-                  path -> TestSet.load(value.getClass().getResourceAsStream(path))
-                      .stream()).collect(toList());
-        }
-     
-        
-        URL docUrl = AaeBIOQA.class.getResource("/questions.json");
-        if (docUrl == null) {
-           throw new IllegalArgumentException("Error opening data/questions.json");
-        }
-        BufferedReader br = new BufferedReader(new InputStreamReader(docUrl.openStream()));
-        while ((sLine = br.readLine()) != null)   {
-          System.out.println(sLine);
-          aCas.setDocumentText(sLine);
-          anAnalysisEngine.process(aCas);
-          aCas.reset();
-        }
-        br.close();
-        br=null;
-        anAnalysisEngine.collectionProcessComplete();
-        anAnalysisEngine.destroy(); 
-        long endTime=System.currentTimeMillis();
-        
-        double totalTime=(endTime-startTime)/1000.0;
-        System.out.println("Total time taken: "+totalTime);
-        
+    }
 
-      }
+    URL docUrl = AaeBIOQA.class.getResource("/questions.json");
+    if (docUrl == null) {
+      throw new IllegalArgumentException("Error opening data/questions.json");
+    }
+    BufferedReader br = new BufferedReader(new InputStreamReader(docUrl.openStream()));
+    while ((sLine = br.readLine()) != null) {
+      System.out.println(sLine);
+      aCas.setDocumentText(sLine);
+      anAnalysisEngine.process(aCas);
+      aCas.reset();
+    }
+    br.close();
+    br = null;
+    anAnalysisEngine.collectionProcessComplete();
+    anAnalysisEngine.destroy();
+    long endTime = System.currentTimeMillis();
+
+    double totalTime = (endTime - startTime) / 1000.0;
+    System.out.println("Total time taken: " + totalTime);
+
+  }
 }
