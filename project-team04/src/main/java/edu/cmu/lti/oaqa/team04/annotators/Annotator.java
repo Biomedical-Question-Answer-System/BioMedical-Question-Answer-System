3611d0f0b5781321b394148aeaec991bfe816c04
rsun07 <rsun2014@gmail.com>
2015-03-28 00:08:19 -0400

    update

--- project-team04/src/main/java/edu/cmu/lti/oaqa/team04/annotators/Annotator.java
+++ project-team04/src/main/java/edu/cmu/lti/oaqa/team04/annotators/Annotator.java
@@ -31,7 +31,6 @@ public class Annotator extends JCasAnnotator_ImplBase {
     try {
       GoPubMedService service = new GoPubMedService("./project.properties");
     } catch (ConfigurationException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
   }
